eee2168da583e6e61da6863b7a0dd3c1d37da45f
sofijaV <sofijavujaklija@yahoo.com>
2021-11-27 17:00:29 +0100

    added use Effect to update weather forecast upon change of coordinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [day, setDay] = useState(null);
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
   function handleResponse(response) {
     setDay(response.data.daily);
     setLoaded(true);
