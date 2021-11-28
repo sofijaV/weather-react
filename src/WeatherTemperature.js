57c5456d09fd9628c7de15367cf0f4d35e905f38
sofijaV <sofijavujaklija@yahoo.com>
2021-11-28 14:56:20 +0100

    changed colors and removed unnecessary lines in css

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,39 +1,11 @@
-import React, { useState } from "react";
-import "./WeatherTemperature.css";
+import React from "react";
+import "./WeatherInfo.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  if (unit === "celsius") {
-    return (
-      <li>
-        {props.celsius}
-        <span className="units">
-          <span onClick={convertToCelsius} className="active">
-            °C |
-          </span>
-          <span onClick={convertToFahrenheit}>°F</span>
-        </span>
-      </li>
-    );
-  } else {
-    return (
-      <li>
-        {Math.round((props.celsius * 9) / 5 + 32)}
-        <span className="units">
-          <span onClick={convertToCelsius}>°C</span>{" "}
-          <span onClick={convertToFahrenheit} className="active">
-            | °F
-          </span>
-        </span>
-      </li>
-    );
-  }
+  return (
+    <li>
+      {props.celsius}
+      <span className="units">°C</span>
+    </li>
+  );
 }
